dffd24a60d6bcbea54828255961bbb5ed055a3e4
saphir746 <deborah.schneider-luftman@crick.ac.uk>
2024-07-23 15:21:23 +0100

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ml Nextflow/24.04.2
+ml Singularity/3.11.3
+
+WORKDIR=/nemo/stp/babs/working/schneid/projects/vinuesac/qian.shen/qs699/
+OUTDIR=${WORKDIR}Immcantation
+INDIR=${WORKDIR}
+
+WORK_DIR=/camp/stp/babs/scratch/schneid/SC24085_Imm_NF/
+
+
+export NXF_SINGULARITY_CACHEDIR=${WORKDIR}/images/cachedir/
+
+nextflow run main.nf -resume \
+	             --WD ${WORKDIR} \
+		     --indir ${INDIR} \
+	             --outdir ${OUTDIR} \
+		     -profile cluster \
+		     -work-dir $WORK_DIR
